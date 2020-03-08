ec3f32e6e67e72831022a63341a68e9fac752ef3
Jesus Bibieca <jesusbibieca52@gmail.com>
2020-03-07 21:31:05 -0400

    missing map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { readRemoteFile } from 'react-papaparse'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import './App.css';
 
 import { handleComplete, FILES } from './scripts/parseCSV';
 
+const MAP_FILE_URL = {
+  confirmed: FILES[0],
+  deaths: FILES[1],
+  recovered: FILES[2]
+};
+
 function App() {
-  const position = [18.4861, -69.98857]
+  const position = [18.4861, -69.98857];
 
   readRemoteFile(FILES[0],
     {

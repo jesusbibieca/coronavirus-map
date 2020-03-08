7ae7d32cc902578a8fd627195174a92692b07fd9
Jesus Bibieca <jesusbibieca52@gmail.com>
2020-03-07 21:27:36 -0400

    adding data parser

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { readRemoteFile } from 'react-papaparse'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import './App.css';
 
+import { handleComplete, FILES } from './scripts/parseCSV';
+
 function App() {
   const position = [18.4861, -69.98857]
+
+  readRemoteFile(FILES[0],
+    {
+      header: true,
+      complete: function(res) {
+        const results = handleComplete(res); //send to state somehow
+      }
+    });
+
   return (
     <Map center={position} zoom={7}>
       <TileLayer
---
+++ src/scripts/parseCSV.js
@@ -0,0 +1,40 @@
+const FILES = [
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv',
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
+];
+
+function handleComplete({ data: entries, errors }) {
+  if (errors) return [];
+  
+  const parsedResults = [];
+  for (const entry of entries) {
+    parsedResults.push(transform(entry));
+  }
+  return parsedResults;
+}
+
+function transform(entry) {
+  return {
+    state: entry['Province/State'],
+    country: entry['Country/Region'],
+    latitude: entry.Lat,
+    longitude: entry.Long,
+    reportedCount: getLastReportedNumber(entry),
+    lastReportDate: getLastReportedDate(entry)
+  };
+}
+
+function getLastReportedDate(entry) {
+  return Object.keys(entry).pop();
+}
+
+function getLastReportedNumber(entry) {
+  const keys = Object.keys(entry);
+  return entry[keys[keys.length - 1]];
+}
+
+module.exports = {
+  handleComplete,
+  FILES
+};
